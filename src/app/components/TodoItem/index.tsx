986eeb6f9f549a8dcd79999252d151ebf6d7864d
joon12864 <wnstn12864@naver.com>
2023-08-22 23:39:35 +0900

    redux

--- src/app/components/TodoItem/index.tsx
+++ src/app/components/TodoItem/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Block from '../Block';
 import Checkbox from '../Checkbox';
+import CircleButton from '../Button/CircleButton';
+import TodoInput from '../TodoInput';
 
 const Box = styled.div`
   display: flex;
@@ -10,6 +12,16 @@ const Box = styled.div`
   width: 100%;
   font-size: 1.2rem;
   border-bottom: 1px solid #eee;
+
+  & > .delete-button {
+    display: none;
+  }
+  &:hover {
+    padding: 10px 15px 10px 25px;
+    & > .delete-button {
+      display: flex;
+    }
+  }
 `;
 
 const TodoContent = styled.span<{ checked: boolean }>`
@@ -27,9 +39,29 @@ const TodoContent = styled.span<{ checked: boolean }>`
 export default function TodoItem({ todo }: { todo: ItodoItem }) {
   return (
     <Box>
-      <Checkbox checked={todo.completed} />
-      <Block marginLeft="10px" />
-      <TodoContent checked={todo.completed}>{todo.content}</TodoContent>
+      <div style={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+        <Checkbox checked={todo.completed} />
+        <Block marginLeft="10px" />
+        {todo.editing ? (
+          <TodoInput />
+        ) : (
+          <TodoContent checked={todo.completed}>{todo.content}</TodoContent>
+        )}
+      </div>
+      <CircleButton
+        className="delete-button"
+        onClick={() => {}}
+        Icon={() => (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
+            <path d="M280-120q-33 0-56.5-23.5T200-200v-520q-17 0-28.5-11.5T160-760q0-17 11.5-28.5T200-800h160q0-17 11.5-28.5T400-840h160q17 0 28.5 11.5T600-800h160q17 0 28.5 11.5T800-760q0 17-11.5 28.5T760-720v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+          </svg>
+        )}
+      />
     </Box>
   );
 }
